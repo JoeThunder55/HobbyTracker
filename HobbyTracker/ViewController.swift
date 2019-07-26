a07005bbd4f84a180bdeb2cc14c78cc114cd4572
Aaron Haughton <AaronJHaughton@outlook.com>
2019-07-25 21:35:38 -0400

    Initial Commit

---
+++ HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Aaron on 7/25/19.
+//  Copyright © 2019 AlphaGrade, INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
